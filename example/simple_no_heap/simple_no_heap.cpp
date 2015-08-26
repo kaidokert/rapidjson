b080439a06d7ec2b90846d7aef22508a26007f44
Kaido Kert <kaidokert@gmail.com>
2015-08-25 23:50:10 -0700

    Reset atrocious formatting

--- example/simple_no_heap/simple_no_heap.cpp
+++ example/simple_no_heap/simple_no_heap.cpp
@@ -11,20 +11,20 @@ using namespace rapidjson;
 using namespace std;
 
 struct CustomAlloc {
-	CustomAlloc(size_t  , unsigned char ) {
-        RAPIDJSON_ASSERT(0);
-	}
-	CustomAlloc() { 
-	}
-	void * Realloc(void * , size_t , size_t ) {
-		RAPIDJSON_ASSERT(0);
-	}
-	static void Free(void *) {
+    CustomAlloc(size_t  , unsigned char ) {
         RAPIDJSON_ASSERT(0);
     }
-	void * Malloc(size_t ) {
+    CustomAlloc() {
+    }
+    void * Realloc(void * , size_t , size_t ) {
         RAPIDJSON_ASSERT(0);
-	}
+    }
+    static void Free(void *) {
+        RAPIDJSON_ASSERT(0);
+    }
+    void * Malloc(size_t ) {
+        RAPIDJSON_ASSERT(0);
+    }
 };
 
 typedef MemoryPoolAllocator<CustomAlloc> myMemoryPoolAlloc;
@@ -37,7 +37,7 @@ void insitu_parse(size_t chunk_size) {
     unsigned char parseBuffer[160];
     myMemoryPoolAlloc valueAllocator(valueBuffer, sizeof(valueBuffer), sizeof(valueBuffer) , &crtAllocPreventer);
     myMemoryPoolAlloc parseAllocator(parseBuffer, sizeof(parseBuffer), sizeof(parseBuffer) , &crtAllocPreventer);
-    
+
     // 1. Parse a JSON string into DOM.
     myDocument d(&valueAllocator, chunk_size, &parseAllocator);
     char json[] = "{\"project\":\"rapidjson\",\"stars\":10}";
@@ -45,7 +45,7 @@ void insitu_parse(size_t chunk_size) {
 
     // 2. Modify it by DOM.
     myValue& s = d["stars"];
-    
+
     // 3. Verify everything worked
     std::cout << "Stars:" << s.GetInt() << std::endl;
     std::cout << "Value alloc used:" << valueAllocator.Size() << std::endl;
